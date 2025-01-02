3ae2ed17b59bc5e1b40ec370fce42d3860e96282
Merbin Russel <reachme@merb.in>
2025-01-02 22:57:08 +0530

    added staging deployment

--- scripts/deploy_compose_staging.sh
+++ scripts/deploy_compose_staging.sh
@@ -23,3 +23,13 @@ if [ ! -f ".env" ]; then
   echo "Error: .env file does not exist, Please create it, use the .env.example as a template"
   exit 1
 fi
+
+mkdir -p logs/catcher logs/deduplicator_stage_one logs/deduplicator_stage_two logs/deduplicator_stage_three
+
+# Load environment variables
+export $(cat .env | xargs)
+
+docker compose -f docker-compose/docker-compose.staging.yml down
+docker system prune -f
+docker image rm docker image rm docker-compose-deduplicator-stage-three docker-compose-catcher docker-compose-deduplicator-stage-two docker-compose-deduplicator-stage-one || true
+docker compose -f docker-compose/docker-compose.staging.yml up
